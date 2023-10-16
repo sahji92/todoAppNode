e4b341f4a7100fb07812c8508404890854f7cac6
deepak sah <sahdeepak330@gmail.com>
2023-10-16 15:34:16 +0530

    update todo feature added

--- index.js
+++ index.js
@@ -15,24 +15,47 @@ mongoose.connect(dbURI)
   //setting view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
 //defining routes
+//get todos from db
 app.get("/", (req, res) => {
   TodoModel.find()
-  .then((result) => {
+  .then(result => {
       res.render("index", {result});
     })
     .catch((err) => console.log(err));
 });
 
+//updated todo show to homepage
+app.get("/todo/:id", (req, res) => {
+  const id=req.params.id
+  TodoModel.findById(id)
+  .then(result => {
+      res.render("todo", {result});
+    })
+    .catch((err) => console.log(err));
+});
+
+//add todo
 app.post("/todo", (req, res) => {
   const todo = new TodoModel(req.body);
   todo.save()
-  .then((result) => {
+  .then(result => {
       res.redirect("/");
     })
     .catch((err) => console.log(err));
 });
 
+//update todo
+app.put("/updateTodo", (req, res) => {
+  const todo = new TodoModel(req.body);
+  TodoModel.findOneAndUpdate({_id:req.body.id},
+    req.body,{new:true})
+    .then(result => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+});
 app.listen(3000);
